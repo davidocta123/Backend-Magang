501f6fff0d353c291eb8cec72efea9d01bbfc0a5
davidocta123 <davi20269ti@student.nurulfikri.ac.id>
2024-08-28 13:46:40 +0700

    revisi source code

--- src/server.ts
+++ src/server.ts
@@ -1,42 +1,52 @@
 import app from './app';
-import promClient from 'prom-client';
+import promClient, { collectDefaultMetrics, Registry } from 'prom-client';
 import { Request, Response } from 'express';
-
 
 
 const PORT = process.env.PORT || 9000;
 
-const register = new promClient.Registry();
-register.setDefaultLabels({
-  app: 'be-bootcamp-kg',
-});
+// Create a new registry
+const register = new Registry();
 
-app.get('/metrics', async (req: Request, res: Response) => {
-  res.setHeader('Content-Type', register.contentType);
-  res.send(await register.metrics());
-});
+// Add the default metrics to the registry
+collectDefaultMetrics({ register });
 
-const httpRequestTimer = new promClient.Histogram({
+// Create a histogram metric
+const httpRequestDurationMicroseconds = new promClient.Histogram({
   name: 'http_request_duration_ms',
   help: 'Duration of HTTP requests in ms',
   labelNames: ['method', 'route', 'code'],
-  // buckets for response time from 0.1ms to 1s
   buckets: [0.1, 5, 15, 50, 100, 200, 300, 400, 500, 1000],
+  registers: [register]
 });
 
-app.get('/tweets', async (req: Request, res: Response) => {
-  const start = Date.now();
-  try {
-    
-  } finally {
-    const responseTimeInMs = Date.now() - start;
-    httpRequestTimer.labels(req.method, req.route.path, res.statusCode.toString()).observe(responseTimeInMs);
-  }
+// Middleware to measure request duration
+app.use((req: Request, res: Response, next) => {
+  const start = process.hrtime();
+  
+  res.on('finish', () => {
+    const duration = process.hrtime(start);
+    const durationMs = duration[0] * 1000 + duration[1] / 1e6;
+    httpRequestDurationMicroseconds
+      .labels(req.method, req.path, res.statusCode.toString())
+      .observe(durationMs);
+  });
+
+  next();
+});
+
+// Metrics endpoint
+app.get('/metrics', async (req: Request, res: Response) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
+
+// Sample route
+app.get('/tweets', (req: Request, res: Response) => {
+  res.send('Hello World');
 });
 
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-  
 });
-
